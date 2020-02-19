1849bcbc859cf9bfede2a4b91274b31f186e665d
Mithil Arun <mithil@platform9.com>
2020-02-19 17:11:02 +0000

    Generate tokens using keystone

--- pf9/provider.go
+++ pf9/provider.go
@@ -1,14 +1,58 @@
 package pf9
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
-
-	"github.com/gophercloud/utils/terraform/auth"
 )
 
 type Config struct {
-	auth.Config
+	DuFQDN           string
+	DuUsername       string
+	DuPassword       string
+	IdentityEndpoint string
+	DuTenantName     string
+	TerraformVersion string
+}
+
+type Domain struct {
+	Id string `json:"id"`
+}
+
+type User struct {
+	Name     string `json:"name"`
+	Domain   Domain `json:"domain"`
+	Password string `json:"password"`
+}
+
+type Password struct {
+	User User `json:"user"`
+}
+
+type Identity struct {
+	Methods  []string `json:"methods"`
+	Password Password `json:"password"`
+}
+
+type Project struct {
+	Name   string `json:"name"`
+	Domain Domain `json:"domain"`
+}
+
+type Scope struct {
+	Project Project `json:"project"`
+}
+
+type Auth struct {
+	Identity Identity `json:"identity"`
+	Scope    Scope    `json:"scope"`
+}
+
+type Request struct {
+	Auth Auth `json:"auth"`
 }
 
 func Provider() terraform.ResourceProvider {
@@ -69,20 +113,53 @@ func init() {
 func configureProvider(d *schema.ResourceData, terraformVersion string) (interface{}, error) {
 	identity_endpoint := "https://" + d.Get("du_fqdn").(string) + "/keystone/v3/auth/tokens"
 	config := Config{
-		auth.Config{
-			IdentityEndpoint:  identity_endpoint,
-			Username:          d.Get("du_username").(string),
-			Password:          d.Get("du_password").(string),
-			DomainName:        "default",
-			ProjectDomainName: "default",
-			TenantName:        d.Get("du_tenant").(string),
-			TerraformVersion:  terraformVersion,
-		},
-	}
-
-	if err := config.LoadAndValidate(); err != nil {
-		return nil, err
+		IdentityEndpoint: identity_endpoint,
+		DuUsername:       d.Get("du_username").(string),
+		DuPassword:       d.Get("du_password").(string),
+		DuTenantName:     d.Get("du_tenant").(string),
+		TerraformVersion: terraformVersion,
+		DuFQDN:           d.Get("du_fqdn").(string),
 	}
 
 	return &config, nil
+}
+
+func generateToken(config *Config) (string, error) {
+	request := &Request{
+		Auth: Auth{
+			Identity: Identity{
+				Methods: []string{"password"},
+				Password: Password{
+					User: User{
+						Name: config.DuUsername,
+						Domain: Domain{
+							Id: "default",
+						},
+						Password: config.DuPassword,
+					},
+				},
+			},
+			Scope: Scope{
+				Project: Project{
+					Name: config.DuTenantName,
+					Domain: Domain{
+						Id: "default",
+					},
+				},
+			},
+		},
+	}
+
+	request_data, err := json.Marshal(request)
+	if err != nil {
+		return "", err
+	}
+
+	resp, errPost := http.Post(config.IdentityEndpoint, "application/json", bytes.NewBuffer(request_data))
+	if errPost != nil {
+		return "", errPost
+	}
+
+	token := resp.Header.Get("X-Subject-Token")
+	return token, nil
 }
