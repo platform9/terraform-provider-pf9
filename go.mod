0168fb36e2179ae42fd0854666596b4f19b0fa59
Mithil Arun <mithil@platform9.com>
2020-02-19 02:49:11 +0000

    Authentication stuff

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/platform9/konform
 
 go 1.13
 
-require github.com/hashicorp/terraform-plugin-sdk v1.7.0
+require (
+	github.com/gophercloud/utils v0.0.0-20200204043447-9864b6f1f12f // indirect
+	github.com/hashicorp/terraform-plugin-sdk v1.7.0
+)
--- pf9/provider.go
+++ pf9/provider.go
@@ -3,10 +3,16 @@ package pf9
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+
+	"github.com/gophercloud/utils/terraform/auth"
 )
 
+type Config struct {
+	auth.Config
+}
+
 func Provider() terraform.ResourceProvider {
-	return &schema.Provider{
+	provider := &schema.Provider{
 		Schema: map[string]*schema.Schema{
 			"du_fqdn": {
 				Type:        schema.TypeString,
@@ -25,12 +31,29 @@ func Provider() terraform.ResourceProvider {
 				Required:    true,
 				Sensitive:   true,
 				Description: descriptions["du_password"],
+				DefaultFunc: schema.EnvDefaultFunc("DU_PASSWORD", ""),
+			},
+			"du_tenant": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: descriptions["du_tenant"],
+				DefaultFunc: schema.EnvDefaultFunc("DU_TENANT", ""),
 			},
 		},
 		ResourcesMap: map[string]*schema.Resource{
 			"pf9_cluster": resourcePF9Cluster(),
 		},
 	}
+
+	provider.ConfigureFunc = func(d *schema.ResourceData) (interface{}, error) {
+		terraformVersion := provider.TerraformVersion
+		if terraformVersion == "" {
+			terraformVersion = "0.11+compatible"
+		}
+		return configureProvider(d, terraformVersion)
+	}
+
+	return provider
 }
 
 var descriptions map[string]string
@@ -41,4 +64,25 @@ func init() {
 		"du_username": "Platform9 Control Plane username.",
 		"du_password": "Platform9 Control Plane password.",
 	}
+}
+
+func configureProvider(d *schema.ResourceData, terraformVersion string) (interface{}, error) {
+	identity_endpoint := "https://" + d.Get("du_fqdn").(string) + "/keystone/v3/auth/tokens"
+	config := Config{
+		auth.Config{
+			IdentityEndpoint:  identity_endpoint,
+			Username:          d.Get("du_username").(string),
+			Password:          d.Get("du_password").(string),
+			DomainName:        "default",
+			ProjectDomainName: "default",
+			TenantName:        d.Get("du_tenant").(string),
+			TerraformVersion:  terraformVersion,
+		},
+	}
+
+	if err := config.LoadAndValidate(); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
 }
